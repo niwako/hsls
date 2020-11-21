af5a82305665bf7aaa46a2653ebb2f60bf26734b
Shad Sharma <shadanan@gmail.com>
2020-11-21 13:39:59 -0800

    Add new relabel function to simplify labels.

--- codebook.py
+++ codebook.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
+import functools
+import pandas as pd
 
 
+@functools.cache
+def variable_labels():
+    labels = {}
+    name = None
+
+    with open("Codebook.txt", "r") as fp:
+        for line in fp:
+            if line.strip() == "":
+                name = None
+                continue
+
+            if line.startswith("Name:"):
+                name = line[12:].strip()
+                continue
+
+            if line.startswith("Label:"):
+                labels[name] = line[12:].strip()
+                continue
+
+    return labels
+
+
+@functools.cache
 def student_encodings():
     DEFAULT = 0
     VALUE = 1
@@ -40,22 +65,20 @@ def is_ordered(*, encoding):
     )
 
 
-def variable_labels():
-    labels = {}
-    name = None
-
-    with open("Codebook.txt", "r") as fp:
-        for line in fp:
-            if line.strip() == "":
-                name = None
-                continue
-
-            if line.startswith("Name:"):
-                name = line[12:].strip()
-                continue
-
-            if line.startswith("Label:"):
-                labels[name] = line[12:].strip()
-                continue
-
-    return labels
+def relabel(df, axis=1):
+    labels = variable_labels()
+    
+    if type(df) == pd.Series:
+        df = df.rename(labels[df.name])
+    
+    if type(df) == pd.DataFrame:
+        if df.index.name in labels:
+            df.index = df.index.rename(labels[df.index.name])
+        if df.columns.name in labels:
+            df.columns = df.columns.rename(labels[df.columns.name])
+        if axis in (0, "index", 2, "both"):
+            df = df.rename(variable_labels(), axis=0)
+        if axis in (1, "column", 2, "both"):
+            df = df.rename(variable_labels(), axis=1)
+    
+    return df
